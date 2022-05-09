ace6d1e5ecef7a0dd49158d8f98780e110e6046d
Poong <peterpoonghaoyuan@gmail.com>
2022-05-09 10:45:48 +0800

    test

--- navfoot.php
+++ navfoot.php
@@ -6,7 +6,7 @@ include 'connection.php';
 
 <!DOCTYPE html>
 <html lang="en">
-
+<div></div>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
